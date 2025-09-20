075246583110de10fb316af4d9e35c135bae9db2
Amr Shoukry <shoukryworkamr1@gmail.com>
2025-09-20 16:11:19 +0300

    Added user credentials for testing

--- config/server.ts
+++ config/server.ts
@@ -1,7 +1,16 @@
 export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  transfer: {
+    token: {
+      salt: env('TRANSFER_TOKEN_SALT', 'a-default-random-string'),
+    },
+    remote: {
+      enabled: true, // Enable remote data transfer
+    },
+  },
   app: {
     keys: env.array('APP_KEYS'),
   },
 });
+
